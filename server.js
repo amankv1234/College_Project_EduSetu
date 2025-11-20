d6df2e3da69aaf252a4eb212fa347109aee3efd1
Aman kumar vishwakrma <186990161+amankv1234@users.noreply.github.com>
2025-11-21 00:36:24 +0530

    intial push for frontend

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// View Engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+// Server
+app.listen(8000, () => {
+  console.log("Server running on http://localhost:8000");
+});
